e281b44642c5d5ce02141e5a73cfb5a6df3e97b2
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-07-14 18:11:48 +0100

    fix association naming

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-  belongs_to :user, foreign_key: 'author_id'
+  belongs_to :author, class_name: 'User'
 end
