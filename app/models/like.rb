5a52fd6b5ab5e6c75bbfb01a1760aebdde369d18
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-07-15 23:26:50 +0100

    add callbacks to update comments/likes/posts count

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+  after_create :update_likes_counter_for_a_post
 
   def update_likes_counter_for_a_post
     Post.find(post_id).update(likes_counter: Post.find(post_id).likes.count)
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy, foreign_key: :author_id
   has_many :comments, dependent: :destroy, foreign_key: :author_id
   has_many :likes, dependent: :destroy, foreign_key: :author_id
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
 
   def top_three_recent_posts
