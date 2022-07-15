df355ac000875871e3b4bfba61adae19fc46f86d
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-07-16 00:00:13 +0100

    add validations to Like and Comment model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,7 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
+  validates :post, presence: true
   after_create :update_likes_counter_for_a_post
 
   def update_likes_counter_for_a_post
