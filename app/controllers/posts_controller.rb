028786b9e4677fd4b86e31d1411878a4844d2505
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-07-23 18:18:05 +0100

    add new and create actions for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,21 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+    render :new
+  end
+
+  def create
+    post = Post.new(params[:post].permit(:title, :text))
+    post.author = current_user
+    if post.save
+      flash[:notice] = "Post was created successfully."
+      redirect_to user_posts_path(current_user, post)
+    else
+      flash.now[:error] = "Post was not created."
+      render :show
+    end
+  end
 end
