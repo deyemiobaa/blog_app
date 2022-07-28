159bcd52cc2fe61d030ec88c56bdf11f3b9116eb
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-07-28 11:06:06 +0100

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,13 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'users#index'
+  root 'devise/sessions#new'
 
   # users and posts routes
   resources :users, only: [:show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:create]
+    resources :posts, only: [:index, :show, :new, :create, :destroy] do
+      resources :comments, only: [:create, :destroy]
       resources :likes, only: [:create]
     end
   end
-
-  # login route
-  # get "/login", to: "sessions#new"
 end
