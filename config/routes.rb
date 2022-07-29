5e10ed2ebb5d2b1699c114f2daf9849d99654533
joel <joelkalema63@gmail.com>
2022-07-29 17:52:43 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,14 @@ Rails.application.routes.draw do
       resources :likes, only: [:create]
     end
   end
+
+  namespace :api, default: { format: :json } do
+    namespace v1 do
+      resources :users, only: [:show] do
+        resources :posts, only: [:show] do
+          resources :comments, only: [:create]
+        end
+      end
+    end
+  end
 end
