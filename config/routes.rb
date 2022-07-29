927724040387390b937f49b0ffee8a755c96c2f0
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-07-30 00:32:46 +0100

    configure custom routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,23 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'devise/sessions#new'
+  root 'devise/sessions#create'
 
   # users and posts routes
-  resources :users, only: [:show] do
+  resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
       resources :comments, only: [:create, :destroy]
       resources :likes, only: [:create]
     end
   end
 
+  # API routes
   namespace :api, default: { format: :json } do
     namespace :v1 do
+      post :auth, to: 'authentication#create'
       resources :users, only: [:show] do
-        resources :posts, only: [:show] do
-          resources :comments, only: [:create]
+        resources :posts, only: [:index] do
+          resources :comments, only: [:index, :create]
         end
       end
     end
